afd37d53662c27b3c45a02f59203824c7c924291
Sandhyashree123 <msandya.sugu@gmail.com>
2023-03-03 15:35:30 +0530

    sandhya

---
+++ src/ja/com/G_project.java
@@ -0,0 +1,12 @@
+package ja.com;
+
+public class G_project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "SAi Ram";
+		System.out.println(s);
+	}
+
+}
---
+++ src/ja/com/firstday.java
@@ -0,0 +1,18 @@
+package ja.com;
+
+public class firstday {
+
+	
+		public void companyName() {
+			System.out.println("Wipro");
+		}
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+			
+				firstday c = new firstday();
+			c.companyName();
+
+	}
+
+}
